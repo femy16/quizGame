d0d65b1056cbffeac17c85e9fda89ffe03b324e1
Femy Stephen <femystephen16@gmail.com>
2018-10-11 16:01:47 +0000

    done for today

--- quiz.py
+++ quiz.py
@@ -11,6 +11,21 @@ def show_menu():
     option=input("Enter options: ")
     return option
 
+def ask_questions():
+    with open("question.txt")as f:
+        questions=f.read().split("\n")[:-1]
+    score=0
+    for q in questions:
+        qlist=q.split("|")
+        # quest=qlist[0]
+        # ans=qlist[1]
+        quest,ans=qlist
+        guess = input(quest)
+        if guess==ans:
+            score+=1
+    print("you scored now {0} out of".format(score))
+
+
 def add_a_question():
   question=input("Enter a Question: ")
   answer=input("Ok then tell me ,"+question.lower()+": ")
@@ -23,7 +38,7 @@ def add_a_question():
 while True:   
     option = show_menu()
     if int(option)== 1:
-        print("run quiz")
+        ask_questions()
     if int(option)== 2:
        add_a_question()
     if int(option) == 3:
